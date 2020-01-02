5b565dd8d132d91a2cc1bcb0ca65483130cb3b4b
ziedbarhoumi1989 <ziedbarhoumi1989@gmail.com>
2020-01-02 11:01:25 +0100

    part 1 complete

--- seed.js
+++ seed.js
@@ -1,11 +1,23 @@
-var data = require('./seed_data.js');
-var mongoose = require('mongoose');
-var Stories = require('./db/models/story.js');
+var data = require("./seed_data.js");
+var dummyData = require("./react-client/dummy_data.js");
+var mongoose = require("mongoose");
+var Stories = require("./db/models/story.js");
 
-mongoose.connect('mongodb://localhost/hackednews');
+mongoose.connect("mongodb://localhost/hackednews");
 
 var seedDb = function(data) {
   // your code here!
+  for (var i = 0; i < data.length; i++) {
+    Stories.insertOne(
+      {
+        id: data[i].id,
+        by: data[i].by.id,
+        title: data[i].title,
+        score: data[i].score
+      },
+      () => console.log("item added")
+    );
+  }
 };
 
 seedDb(data);
--- server/index.js
+++ server/index.js
@@ -1,23 +1,23 @@
-var express = require('express');
-var bodyParser = require('body-parser');
-var storyRouter = require('./routers/story.js');
-var mongoose = require('mongoose');
+var express = require("express");
+var bodyParser = require("body-parser");
+var storyRouter = require("./routers/story.js");
+var mongoose = require("mongoose");
 
 var app = express();
 
-mongoose.connect('mongodb://localhost/hackednews');
+mongoose.connect("mongodb://localhost/hackednews");
 
 app.use(bodyParser.json());
 
 // UNCOMMENT FOR REACT
-// app.use(express.static(__dirname + '/../react-client/dist'));
+app.use(express.static(__dirname + "/../react-client/dist"));
 
 // UNCOMMENT FOR ANGULAR
 // app.use(express.static(__dirname + '/../angular-client'));
 // app.use(express.static(__dirname + '/../node_modules'));
 
-app.use('/api/story', storyRouter);
+app.use("/api/story", storyRouter);
 
 app.listen(8000, function() {
-  console.log('listening on port 8000');
+  console.log("listening on port 8000");
 });
